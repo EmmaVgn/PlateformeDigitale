09363c9e419931efb63d33fb5348d38cd0531763
EmmaVgn <emma.vigneron@outlook.fr>
2025-03-31 17:48:28 +0200

    modif de profile

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -5,15 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\UserFormationRepository;
 
 final class DashboardController extends AbstractController{
-    #[Route('/profile', name: 'app_dashboard')]
-    public function index(): Response
+    #[Route('/profile', name: 'profile')]
+    public function index(UserFormationRepository $userFormationRepository): Response
     {
         $this->denyAccessUnlessGranted('ROLE_USER');
-        
+
+        $user = $this->getUser();
+    
+        $inscriptions = $userFormationRepository->findBy(['user' => $user]);
+    
         return $this->render('dashboard/index.html.twig', [
-            'controller_name' => 'DashboardController',
+            'inscriptions' => $inscriptions,
         ]);
     }
 }
--- src/Controller/FormationController.php
+++ src/Controller/FormationController.php
@@ -31,12 +31,60 @@ class FormationController extends AbstractController
             throw $this->createNotFoundException('Formation introuvable.');
         }
 
+        $inscription = null;
+        if ($this->getUser()) {
+            foreach ($formation->getInscriptions() as $item) {
+                if ($item->getUser() === $this->getUser()) {
+                    $inscription = $item;
+                    break;
+                }
+            }
+        }
+
         return $this->render('formation/show.html.twig', [
             'formation' => $formation,
+            'inscription' => $inscription,
         ]);
     }
 
-    #
+    #[Route('/formations/{slug}/inscription', name: 'formation_inscription')]
+    public function inscription(
+        string $slug,
+        FormationRepository $formationRepository,
+        EntityManagerInterface $em
+    ): Response {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+    
+        $formation = $formationRepository->findOnePublishedBySlug($slug);
+    
+        if (!$formation) {
+            throw $this->createNotFoundException('Formation non trouvée.');
+        }
+    
+        $user = $this->getUser();
+    
+        // Vérifie si l’utilisateur est déjà inscrit
+        foreach ($formation->getInscriptions() as $inscription) {
+            if ($inscription->getUser() === $user) {
+                $this->addFlash('warning', 'Vous êtes déjà inscrit à cette formation.');
+                return $this->redirectToRoute('formation_show', ['slug' => $slug]);
+            }
+        }
+    
+        $inscription = new UserFormation();
+        $inscription->setUser($user);
+        $inscription->setFormation($formation);
+        $inscription->setProgression(0);
+        $inscription->setIsCompleted(false);
+    
+        $em->persist($inscription);
+        $em->flush();
+    
+        $this->addFlash('success', 'Vous êtes maintenant inscrit à cette formation.');
+    
+        return $this->redirectToRoute('formation_show', ['slug' => $slug]);
+    }
+
 
 }
 
